13a42559dd935db8cc6310f1bd25156fd60d835f
Andre Wagner <andre.nho@gmail.com>
2019-02-12 19:21:55 -0200

    .

---
+++ terraform/alb.tf
@@ -0,0 +1,23 @@
+resource "aws_lb" "alb" {
+    name                = "backend-alb"
+    internal            = false
+    load_balancer_type  = "application"
+    subnets             = ["${data.aws_subnet.public.id}"]
+    security_groups     = [
+        "${aws_security_group.allow_8080.id}",
+        "${aws_security_group.allow_outbound.id}"
+    ]
+
+    enable_deletion_protection = false
+
+    # TODO - logs
+
+    tags {
+        Name        = "backend-alb"
+        Creator     = "backend"
+        Environment = "${var.env}"
+        Description = "Backend application load balancer"
+    }
+}
+
+# vim:ts=4:sw=4:sts=4:expandtab:syntax=conf
