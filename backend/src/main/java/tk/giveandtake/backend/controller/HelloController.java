c26c72631215d49426ba8bd97f8b5875a3d83ff1
andrenho <xmate156>
2019-02-12 12:28:27 -0200

    .

--- backend/src/main/java/tk/giveandtake/backend/controller/HelloController.java
+++ backend/src/main/java/tk/giveandtake/backend/controller/HelloController.java
@@ -4,10 +4,35 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
 @RestController
 public class HelloController {
     @GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    String hello() {
+    String hello()  {
         return "world";
+    }
+
+    @GetMapping(value = "/ip", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    String ip() throws SocketException {
+        String ip;
+        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+        while (interfaces.hasMoreElements()) {
+            NetworkInterface iface = interfaces.nextElement();
+            // filters out 127.0.0.1 and inactive interfaces
+            if (iface.isLoopback() || !iface.isUp())
+                continue;
+
+            Enumeration<InetAddress> addresses = iface.getInetAddresses();
+            while(addresses.hasMoreElements()) {
+                InetAddress addr = addresses.nextElement();
+                ip = addr.getHostAddress();
+                return ip;
+            }
+        }
+        return "not found";
     }
 }
