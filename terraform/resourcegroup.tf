0d602123dfb16efa42bbc6f1b6f66b9a0b64f6bf
André Wagner <andre.nho@gmail.com>
2019-02-12 15:47:49 -0200

    .

--- terraform/resourcegroup.tf
+++ terraform/resourcegroup.tf
@@ -1,6 +1,6 @@
-resource "aws_resourcegroups_group" "resg-backend" {
-    name = "backend"
-    description = "Resources built for the backend."
+resource "aws_resourcegroups_group" "resg-backend-devl" {
+    name = "backend-devl"
+    description = "Resources built for the backend (devl)."
     
     resource_query {
     query = <<JSON
@@ -10,6 +10,33 @@ resource "aws_resourcegroups_group" "resg-backend" {
     {
       "Key": "Creator",
       "Values": ["backend"]
+    },
+    {
+      "Key": "Environment",
+      "Values": ["devl"]
+    }
+  ]
+}
+JSON
+  }
+}
+
+resource "aws_resourcegroups_group" "resg-backend-master" {
+    name = "backend-master"
+    description = "Resources built for the backend (master)."
+    
+    resource_query {
+    query = <<JSON
+{
+  "ResourceTypeFilters": ["AWS::AllSupported"],
+  "TagFilters": [
+    {
+      "Key": "Creator",
+      "Values": ["backend"]
+    },
+    {
+      "Key": "Environment",
+      "Values": ["master"]
     }
   ]
 }
