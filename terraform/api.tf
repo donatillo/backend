40277c0fde9f8d7e24266975b9834d4d28ef6aea
André Wagner <andre.nho@gmail.com>
2019-02-14 16:35:51 -0200

    .

--- terraform/api.tf
+++ terraform/api.tf
@@ -11,7 +11,7 @@ resource "aws_api_gateway_resource" "proxy" {
 
 resource "aws_api_gateway_method" "proxy" {
     rest_api_id     = "${aws_api_gateway_rest_api.api.id}"
-    parent_id       = "${aws_api_gateway_rest_api.api.root_resource_id}"
+    resource_id     = "${aws_api_gateway_resource.proxy.id}"
     http_method     = "ANY"
     authorization   = "NONE"
 }
