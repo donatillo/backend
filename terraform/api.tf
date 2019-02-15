509d01a94ef11a95491d541fb6d6cb4d17227854
Andre Wagner <andre.nho@gmail.com>
2019-02-15 21:59:16 -0200

    .

--- terraform/api.tf
+++ terraform/api.tf
@@ -4,29 +4,11 @@ resource "aws_api_gateway_rest_api" "api" {
     body        = "${file("../api.yaml")}"
 }
 
-/*
-resource "aws_api_gateway_resource" "proxy" {
+resource "aws_api_gateway_deployment" "api_deploy" {
+    depends_on = [ "aws_api_gateway_rest_api.api" ]
+
     rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-    parent_id   = "${aws_api_gateway_rest_api.api.root_resource_id}"
-    path_part   = "{proxy+}"
+    stage_name  = "devl"
 }
-
-resource "aws_api_gateway_method" "proxy" {
-    rest_api_id     = "${aws_api_gateway_rest_api.api.id}"
-    resource_id     = "${aws_api_gateway_resource.proxy.id}"
-    http_method     = "ANY"
-    authorization   = "NONE"
-}
-
-resource "aws_api_gateway_integration" "http_request" {
-    rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-    resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-    http_method = "${aws_api_gateway_method.proxy.http_method}"
-
-    type        = "HTTP_PROXY"
-    uri         = "http://${var.subdomain}.${var.domain}:8080/{proxy}"
-    integration_http_method = "ANY"
-}
-*/
 
 # vim:ts=4:sw=4:sts=4:expandtab:syntax=conf
