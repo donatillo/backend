f26273bad5b5374e18caa1ee7742c0fe24ec9f48
Andre Wagner <andre.nho@gmail.com>
2019-02-16 13:21:22 -0200

    .

--- terraform/api.tf
+++ terraform/api.tf
@@ -15,14 +15,12 @@ resource "aws_api_gateway_rest_api" "api" {
 }
 
 # API deployment
-/*
 resource "aws_api_gateway_deployment" "api_deploy" {
     depends_on = [ "aws_api_gateway_rest_api.api" ]
 
     rest_api_id = "${aws_api_gateway_rest_api.api.id}"
     stage_name  = "${var.env}"
 }
-*/
 
 # API custom domain name
 
