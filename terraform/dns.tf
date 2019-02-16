0722afd149ad1d8716341a2b4fd5f3533fc3be21
Andre Wagner <andre.nho@gmail.com>
2019-02-16 13:18:12 -0200

    .

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -39,6 +39,7 @@ resource "aws_route53_record" "public-ipv4" {
     alias {
         name        = "${aws_api_gateway_domain_name.api_domain.cloudfront_domain_name}"
         zone_id     = "${aws_api_gateway_domain_name.api_domain.cloudfront_zone_id}"
+        evaluate_target_health = true
     }
 }
 
@@ -49,6 +50,7 @@ resource "aws_route53_record" "public-ipv6" {
     alias {
         name        = "${aws_api_gateway_domain_name.api_domain.cloudfront_domain_name}"
         zone_id     = "${aws_api_gateway_domain_name.api_domain.cloudfront_zone_id}"
+        evaluate_target_health = true
     }
 }
 
