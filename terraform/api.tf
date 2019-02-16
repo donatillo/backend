c98df215e332d1f779a1175ed694af2e369ffc0e
Andre Wagner <andre.nho@gmail.com>
2019-02-15 22:07:56 -0200

    .

--- terraform/api.tf
+++ terraform/api.tf
@@ -2,6 +2,8 @@ resource "aws_api_gateway_rest_api" "api" {
     name        = "giveandtake-backend"
     description = "Give and Take Application"
     body        = "${file("../api.yaml")}"
+
+    # TODO - add tags
 }
 
 resource "aws_api_gateway_deployment" "api_deploy" {
@@ -9,6 +11,8 @@ resource "aws_api_gateway_deployment" "api_deploy" {
 
     rest_api_id = "${aws_api_gateway_rest_api.api.id}"
     stage_name  = "devl"
+
+    # TODO - add tags
 }
 
 # vim:ts=4:sw=4:sts=4:expandtab:syntax=conf
