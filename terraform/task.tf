55d17ce92fb20941e85dd64ab590e9794d7fb556
Andre Wagner <andre.nho@gmail.com>
2019-02-17 12:36:37 -0300

    .

--- terraform/task.tf
+++ terraform/task.tf
@@ -1,11 +1,20 @@
+# parse task file
+data "template_file" "task" {
+    template    = "${file("task.json")}"
+    vars {
+        env     = "${var.env}"
+    }
+}
+
+# create task definition
 resource "aws_ecs_task_definition" "service" {
-    family          = "backend-app"
+    family          = "backend-app-${var.env}"
     network_mode    = "awsvpc"
     requires_compatibilities = ["FARGATE"]
 	cpu             = 256 
 	memory          = 512
     execution_role_arn = "arn:aws:iam::324139215624:role/ecsTaskExecutionRole"
-    container_definitions = "${file("task.json")}"
+    container_definitions = "${data.template_file.task.rendered}"
 
     tags {
         Name        = "backend-taskdefs"
