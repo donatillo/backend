13d608405808275a4dd2f55f336eac580cab8c1f
Andre Wagner <andre.nho@gmail.com>
2019-02-17 09:28:07 -0300

    .

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -48,6 +48,17 @@ data "aws_acm_certificate" "cert" {
 
 resource "aws_lb_listener" "backend-listener" {
     load_balancer_arn   = "${aws_lb.alb.arn}"
+    port                = 80
+    protocol            = "HTTP"
+
+    default_action {
+        target_group_arn = "${aws_lb_target_group.backend-target.arn}"
+        type             = "forward"
+    }
+}
+
+resource "aws_lb_listener" "backend-listener" {
+    load_balancer_arn   = "${aws_lb.alb.arn}"
     port                = 443
     protocol            = "HTTPS"
     ssl_policy          = "ELBSecurityPolicy-2016-08"
