c385b691eb88ff8fd3f2ce6941cc5bbb83975873
Andre Wagner <andre.nho@gmail.com>
2019-02-22 19:52:37 -0300

    .

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -48,14 +48,14 @@ data "aws_acm_certificate" "cert" {
 
 resource "aws_lb_listener" "backend-listener" {
     load_balancer_arn   = "${aws_lb.alb.arn}"
-    port                = 80
-    protocol            = "HTTP"
-    # ssl_policy          = "ELBSecurityPolicy-2016-08"
-	# certificate_arn     = "${data.aws_acm_certificate.cert.arn}"
+    port                = 443
+    protocol            = "HTTPS"
+    ssl_policy          = "ELBSecurityPolicy-2016-08"
+	certificate_arn     = "${data.aws_acm_certificate.cert.arn}"
 
     default_action {
-        target_group_arn = "${aws_lb_target_group.backend-target.arn}"
         type             = "forward"
+        target_group_arn = "${aws_lb_target_group.backend-target.arn}"
     }
 }
 
