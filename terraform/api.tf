851e5dc7fda4910ccc11c878ad10513df3b1b900
Andre Wagner <andre.nho@gmail.com>
2019-02-22 20:06:16 -0300

    .

--- terraform/api.tf
+++ terraform/api.tf
@@ -10,8 +10,8 @@ data "template_file" "yaml" {
 
 # REST API
 resource "aws_api_gateway_rest_api" "api" {
-    name        = "giveandtake-backend-${var.env}"
-    description = "Give and Take Application"
+    name        = "${var.basename}-backend-${var.env}"
+    description = "${var.basename} application - ${var.env}"
     body        = "${data.template_file.yaml.rendered}"
 }
 
@@ -28,24 +28,12 @@ resource "aws_api_gateway_base_path_mapping" "mapping" {
 	domain_name = "${aws_api_gateway_domain_name.api_domain.domain_name}"
 }
 
-# client certificate
-resource "aws_api_gateway_client_certificate" "client_certificate" {
-	description = "Certificate for backend running at ${var.subdomain}.${var.domain}"
-}
-
 # API deployment
 resource "aws_api_gateway_deployment" "api_deploy" {
     depends_on = [ "aws_api_gateway_rest_api.api" ]
 
     rest_api_id = "${aws_api_gateway_rest_api.api.id}"
-    stage_name  = "temp"
-}
-
-resource "aws_api_gateway_stage" "test" {
-    stage_name            = "${var.env}"
-    rest_api_id           = "${aws_api_gateway_rest_api.api.id}"
-    deployment_id         = "${aws_api_gateway_deployment.api_deploy.id}"
-	client_certificate_id = "${aws_api_gateway_client_certificate.client_certificate.id}"
+    stage_name  = "devl"
 }
 
 # vim:ts=4:sw=4:sts=4:expandtab:syntax=conf
