856f6851e3944506f8d24546e0a6e92c337998ec
Andre Wagner <andre.nho@gmail.com>
2019-02-24 09:22:14 -0300

    .

--- backend/backend.py
+++ backend/backend.py
@@ -5,42 +5,14 @@ import socket
 
 app = Flask(__name__)
 api = Api(app)
-cors = CORS(app)
+cors = CORS(app, resources={r"/*": {"origins": ["http://localhost:3000"]}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/tokensignin')
-@cross_origin()
-def signin():
-    return dict(request.headers)
+class SignIn(Resource):
+    def post(self):
+        return 'ok'
 
-class HelloWorld(Resource):
-    def get(self):
-        #return { 'hello': 'world' }
-        return dict(request.headers)
-
-api.add_resource(HelloWorld, '/')
-
-class Version(Resource):
-    def get(self):
-        #return { 'hello': 'world' }
-        return '0.1'
-
-api.add_resource(Version, '/version')
-
-class IP(Resource):
-    def get(self):
-        ips = []
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        try:
-            s.connect(('10.255.255.255', 1))
-            ips.append(s.getsockname()[0])
-        except:
-            ips.append('127.0.0.1')
-        finally:
-            s.close()
-        return ips
-
-api.add_resource(IP, '/ip')
+api.add_resource(SignIn, '/tokensignin')
 
 if __name__ == '__main__':
     app.run(debug=True)
